7e83e8cac137f6b3ecc100c7d1a3b031d1802e07
shravanhebbar <shravan.intern@axisrooms.com>
2018-01-14 16:32:40 +0530

    hasmap

---
+++ hasmap3.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+public class hasmap3{
+	public static void main(String args[]) {
+		
+	
+	Set<Integer> s=new HashSet<Integer>();
+	int a,b;
+	Scanner ss=new Scanner(System.in);
+	a=ss.nextInt();
+	b=ss.nextInt();
+	for(int i=0;i<a;i++) {
+		s.add(ss.nextInt());
+	}
+	//Iterator<Integer> itr=s.iterator();  
+	//Iterator<Integer> itr1=s.iterator();
+	int flag=0;
+	
+	   for (Integer key1: s){
+		   if(flag==0) {
+		   for (Integer key: s) {
+			
+			if((key1+key)==b) {
+				System.out.println("Yes");
+				flag=1;
+				break;
+			}
+			
+			
+		}
+		
+	}
+	}
+	
+	
+	
+	
+	
+	
+}
+}
