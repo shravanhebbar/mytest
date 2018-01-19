ecb1ac7921fa5149bfaa4f8b905d379edb72d637
shravanhebbar <shravan.intern@axisrooms.com>
2018-01-19 15:14:02 +0530

    java8thread

---
+++ java8thread3.java
@@ -0,0 +1,36 @@
+package java8threading;
+
+import java.lang.*;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Future;
+
+import java.io.*;
+class java8thread3{
+	public static void main(String[]  args) throws InterruptedException, ExecutionException {
+	Callable<Integer> task = () -> {
+	    try {
+	        TimeUnit.SECONDS.sleep(1);
+	        return 123;
+	    }
+	    catch (InterruptedException e) {
+	        throw new IllegalStateException("task interrupted", e);
+	    }
+	};
+	ExecutorService executor = Executors.newFixedThreadPool(1);
+	Future<Integer> future = executor.submit(task);
+
+	System.out.println("future done? " + future.isDone());
+
+	Integer result = future.get();
+
+	System.out.println("future done? " + future.isDone());
+	System.out.print("result: " + result);
+	
+
+}
+}
