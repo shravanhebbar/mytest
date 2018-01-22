9dcdcda8990e6f8c9f5868470d07aa012023a7ec
shravanhebbar <shravan.intern@axisrooms.com>
2018-01-22 18:10:07 +0530

    java8 thread

---
+++ java8thread6.java
@@ -0,0 +1,30 @@
+package java8threading;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class java8thread6 {
+
+    public static void main(String args[]) {
+       ExecutorService service = Executors.newFixedThreadPool(10);
+       for (int i =0; i<100; i++){
+           service.submit(new Task(i));
+       }
+    }
+}
+
+final class Task implements Runnable{
+    private int taskId;
+
+    public Task(int id){
+        this.taskId = id;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Task ID : " + this.taskId +" performed by "
+            + Thread.currentThread().getName());
+    }
+
+}
