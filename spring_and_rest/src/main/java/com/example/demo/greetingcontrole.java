9bfbfb4ff9c868bf7334a6c931b61e39b811517c
shravanhebbar <35368865+shravanhebbar@users.noreply.github.com>
2018-01-31 17:27:43 +0530

    rest and spring

---
+++ spring_and_rest/src/main/java/com/example/demo/greetingcontrole.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class greetingcontrole {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+        @RequestMapping("/greeting/1")
+        public greeting greeting1() {
+            return new greeting(counter.incrementAndGet(),
+                                String.format(template, "shravan"));
+        
+}
+}
