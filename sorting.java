7f5d00e3927817b6d2831039b1f4c178a2a5e920
shravanhebbar <35368865+shravanhebbar@users.noreply.github.com>
2018-02-06 18:21:51 +0530

    Re-edited

--- sorting.java
+++ sorting.java
@@ -21,9 +21,9 @@ public class sorting {
      
    }
    
-   //sort using java 7
    
-   //sort using java 8
+   
+   
    String s1,s2;
    private void sortUsingJava8(List<String> names) {
       Collections.sort(names, (s1, s2) -> s1.compareTo(s2));
